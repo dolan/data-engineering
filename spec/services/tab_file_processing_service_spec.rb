10b6cfb533eddb465581f10783ac3eff00399c22
Dave Dolan <dave.dolan@entropi.co>
2014-03-21 18:57:28 -0400

    failing tests for the TabFileProcessingService

--- spec/services/tab_file_processing_service_spec.rb
+++ spec/services/tab_file_processing_service_spec.rb
@@ -1,20 +1,90 @@
 require 'spec_helper'
 
 describe TabFileProcessingService do
+
+  before(:each) do
+    @data_file_upload = create(:data_file_upload)
+    @tab_file_processing_service = TabFileProcessingService.new(@data_file_upload)
+  end
   context "initializer" do
-    it "creates an instance when passed a data_file_upload object"
-    it "creates an instance when passed a data_file_upload id"
+    it "creates an instance when passed a data_file_upload object" do
+      @service = TabFileProcessingService.new(@data_file_upload)
+      expect(@service).not_to be_nil
+    end
+    it "creates an instance when passed a data_file_upload id" do
+      @service = TabFileProcessingService.new(@data_file_upload.id)
+      expect(@service).not_to be_nil
+    end
   end
 
   context "instance method" do
     describe ":process parses the file and creates the normalized data" do
-      it "processes the file"
-      it "creates the proper number of purchase items"
-      it "does not duplicate items"
-      it "does not duplicate merchants"
-      it "does not duplicate customers"
-      it "will not process the same file twice"
-      it "will set the total income for the data file"
+
+      it "sets the processed_at time stamp on the upload object" do
+        describe "starts out nil" do
+          expect(@data_file_upload.processed_at).to be_nil
+        end
+
+        @tab_file_processing_service.process
+
+        describe "is not nil after processing" do
+          expect(@data_file_upload.processed_at).not_to be_nil
+        end
+      end
+
+      it "processes the file" do
+        result = @tab_file_processing_service.process
+        expect(result).to be_truthy
+      end
+
+      it "creates the proper number of purchase items" do
+        expected_count = 4 # sample file has 4 items
+        result = @tab_file_processing_service.process
+        expect(result.length).to eq(expected_count)
+      end
+
+      it "does not duplicate items" do
+
+        # the sample input data has two line items for this product
+        item_string = "$20 Sneakers for $5"
+        result = @tab_file_processing_service.process
+
+        # should still be in the database only once
+        expect(Item.where(description: item_string).count).to eq(1)
+      end
+
+      it "does not duplicate merchants" do
+
+        # the sample input data has two line items from this merchant
+        merchant_string = "Sneaker Store Emporium"
+        result = @tab_file_processing_service.process
+
+        # should still be in the database only once
+        expect(Merchant.where(name: merchant_string).count).to eq(1)
+      end
+
+      it "does not duplicate customers" do
+
+        # the sample input data has two line items from this customer
+        customer_string = "Snake Plissken"
+        result = @tab_file_processing_service.process
+
+        # should still be in the database only once
+        expect(Customer.where(name: customer_string).count).to eq(1)
+      end
+
+      it "will not process the same file twice" do
+        first_result = @tab_file_processing_service.process # first pass
+        second_result = @tab_file_processing_service.process # second pass
+        expect(second_result).to be_falsey
+      end
+
+      it "will set the total income for the data file" do
+        initial_value = @data_file_upload.total_income
+        @tab_file_processing_service.process
+        new_value = @data_file_upload.total_income
+        expect(new_value).to be_greater_than(initial_value)
+      end
     end
   end
 end
