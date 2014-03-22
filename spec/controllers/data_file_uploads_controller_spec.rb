2a42e225424c503d4f68ce0a323b235b8c332998
Dave Dolan <dave.dolan@entropi.co>
2014-03-21 22:02:25 -0400

    foozle

--- spec/controllers/data_file_uploads_controller_spec.rb
+++ spec/controllers/data_file_uploads_controller_spec.rb
@@ -20,11 +20,5 @@ require 'spec_helper'
 
 describe DataFileUploadsController do
 
-describe "GET 'index'" do
-  it "should be successful" do
-    get 'index'
-    response.should be_success
-  end
-end
 
 end
